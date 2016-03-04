822b304b84e16f54f0b2b6fbd6853cf17d0d2bd3
lyndsey browning <lbrowning86@gmail.com>
2016-03-04 13:29:46 +0000

    Setup plugin layout

--- js/select-all.js
+++ js/select-all.js
@@ -1,18 +1,27 @@
-$.fn.selectAll = function () {
+(function ($) {
+  $.fn.selectAll = function (opts) {
     return this.each(function () {
-        var self = $(this),
-            target = self.data('target'),
-            totalChks = $('input' + target).length,
-            markChked = function () {
-                var numChked = $('input' + target + ":checked").length;
-                self.prop('checked', (totalChks > 0) ? (numChked === totalChks) : false);
-            };
 
-        self.on('click', function () {
-            $(target).prop('checked', self.is(':checked'));
-        });
+      var options = $.extend({
+        showCount: false
+      }, opts);
 
-        $(target).on('click', markChked);
-        markChked();
+      var self = $(this),
+          target = self.data('target'),
+          totalChks = $('input' + target).length,
+          markChked = function () {
+            var numChked = $('input' + target + ":checked").length;
+            self.prop('checked', (totalChks > 0) ? (numChked === totalChks) : false);
+          };
+
+      self.on('click', function () {
+        $(target).prop('checked', self.is(':checked'));
+      });
+
+      $(target).on('click', markChked);
+      markChked();
     });
-};
+  };
+})(jQuery);
+
+
